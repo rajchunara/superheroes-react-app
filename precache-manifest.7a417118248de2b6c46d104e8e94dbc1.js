ace66e04d3608d982f8cb5f25df0a582c6635034
rajchunara <rajchunara777@gmail.com>
2020-07-04 13:16:13 -0400

    Updates

---
+++ precache-manifest.7a417118248de2b6c46d104e8e94dbc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bdb0954b199f9b16634ec968bb3ba4",
+    "url": "/superheroes-react-app/index.html"
+  },
+  {
+    "revision": "6de4185085d645800439",
+    "url": "/superheroes-react-app/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "af5a0c0c0c58bf3868f6",
+    "url": "/superheroes-react-app/static/css/main.353ddeea.chunk.css"
+  },
+  {
+    "revision": "6de4185085d645800439",
+    "url": "/superheroes-react-app/static/js/2.b8c6d07b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/superheroes-react-app/static/js/2.b8c6d07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5a0c0c0c58bf3868f6",
+    "url": "/superheroes-react-app/static/js/main.2827d7fb.chunk.js"
+  },
+  {
+    "revision": "4714f18590a038244078",
+    "url": "/superheroes-react-app/static/js/runtime-main.6f3b7ae5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/superheros-react-app/precache-manifest.0f29c3c6c521e291cc099d59efd6ab99.js"
+  "/superheroes-react-app/precache-manifest.7a417118248de2b6c46d104e8e94dbc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superheros-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superheroes-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
