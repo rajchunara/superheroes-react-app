eafd96f2232040431739e7382e94997ce6c96d87
rajchunara <rajchunara777@gmail.com>
2020-07-04 13:00:36 -0400

    Updates

---
+++ precache-manifest.0f29c3c6c521e291cc099d59efd6ab99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba54c14cc46c673a256a3683010b683",
+    "url": "/superheros-react-app/index.html"
+  },
+  {
+    "revision": "6de4185085d645800439",
+    "url": "/superheros-react-app/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0fe9492efa10073e940c",
+    "url": "/superheros-react-app/static/css/main.353ddeea.chunk.css"
+  },
+  {
+    "revision": "6de4185085d645800439",
+    "url": "/superheros-react-app/static/js/2.b8c6d07b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/superheros-react-app/static/js/2.b8c6d07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe9492efa10073e940c",
+    "url": "/superheros-react-app/static/js/main.68ad304d.chunk.js"
+  },
+  {
+    "revision": "3ee92be005c41ed9bd90",
+    "url": "/superheros-react-app/static/js/runtime-main.95f3745b.js"
+  }
+]);
